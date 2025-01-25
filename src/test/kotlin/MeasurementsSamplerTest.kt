fbb266eba6587cb83db262e28e492cadfaa29767
achelyadina <anastasia.chelyadina@dataart.com>
2025-01-25 14:42:36 +0200

    change unit test to cover sorting order check

--- src/test/kotlin/MeasurementsSamplerTest.kt
+++ src/test/kotlin/MeasurementsSamplerTest.kt
@@ -14,11 +14,11 @@ class MeasurementSamplerTest {
         Measurement(LocalDateTime.parse(time), value, type)
 
     @Test
-    fun `test sampling performed on given data`() {
+    fun `test sampling performed on given data and output is sorted`() {
         val measurements = listOf(
+            measurement("2017-01-03T10:09:07", 35.01, MeasurementType.TEMP),
             measurement("2017-01-03T10:04:45", 35.79, MeasurementType.TEMP),
             measurement("2017-01-03T10:01:18", 98.78, MeasurementType.SPO2),
-            measurement("2017-01-03T10:09:07", 35.01, MeasurementType.TEMP),
             measurement("2017-01-03T10:03:34", 96.49, MeasurementType.SPO2),
             measurement("2017-01-03T10:02:01", 35.82, MeasurementType.TEMP),
             measurement("2017-01-03T10:05:00", 97.17, MeasurementType.SPO2),
